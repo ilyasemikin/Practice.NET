e9d2a5d694b7083d504959255315df54579db46e
ilyasemikin <iasemikin@gmail.com>
2022-02-05 17:59:24 +0300

    LeetCode 739 solution

---
+++ LeetCode/Solution739.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode;
+
+public class Solution739
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var answer = new int[temperatures.Length];
+        var stack = new Stack<int>();
+
+        for (var i = temperatures.Length - 1; i >= 0; i--)
+        {
+            while (stack.Count > 0 && temperatures[stack.Peek()] <= temperatures[i])
+                stack.Pop();
+
+            if (stack.Count > 0)
+                answer[i] = stack.Peek() - i;
+            stack.Push(i);
+        }
+
+        return answer;
+    }
+}
---
+++ Tests/LeetCode.Tests/Solution739_Tests.cs
@@ -0,0 +1,23 @@
+using NUnit.Framework;
+
+namespace LeetCode.Tests;
+
+public class Solution739_Tests
+{
+    private Solution739? _solution;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _solution = new Solution739();
+    }
+
+    [TestCase(new int[] { 73, 74, 75, 71, 69, 72, 76, 73 }, new int[] { 1, 1, 4, 2, 1, 1, 0, 0 })]
+    [TestCase(new int[] { 30, 40, 50, 60 }, new int[] { 1, 1, 1, 0 })]
+    [TestCase(new int[] { 30, 60, 90 }, new int[] { 1, 1, 0 })]
+    public void Test(int[] input, int[] expected)
+    {
+        var temperatures = _solution!.DailyTemperatures(input);
+        CollectionAssert.AreEqual(expected, temperatures);
+    }
+}
