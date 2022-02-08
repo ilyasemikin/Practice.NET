f64a34a34917fb753ef594b5e78fe44500fc023a
ilyasemikin <iasemikin@gmail.com>
2022-02-08 20:40:14 +0300

    NumberOfDigits + tests

---
+++ Algorithms/Algorithms/Integer.cs
@@ -0,0 +1,14 @@
+﻿namespace Algorithms;
+
+public static class Integer
+{
+    public static int NumberOfDigits(long num)
+    {
+        return num switch
+        {
+            0 => 1,
+            long.MinValue => 19,
+            _ => (int) Math.Log10(Math.Abs(num)) + 1
+        };
+    }
+}
---
+++ Tests/Algorithms.Tests/IntegerTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms.Tests;
+
+public class IntegerTests
+{
+    [TestCase(0, 1)]
+    [TestCase(1, 1)]
+    [TestCase(100, 3)]
+    [TestCase(10000, 5)]
+    [TestCase(9223372036854775807, 19)]
+    public void NumberOfDigitsTest(long num, int expected)
+    {
+        Assert.AreEqual(expected, Integer.NumberOfDigits(num));
+    }
+
+    [TestCase(-1, 1)]
+    [TestCase(-100, 3)]
+    [TestCase(-10000, 5)]
+    [TestCase(-1000000, 7)]
+    [TestCase(-9223372036854775808, 19)]
+    public void NumberOfDigitsOfNegativeNumberTest(long num, int expected)
+    {
+        Assert.AreEqual(expected, Integer.NumberOfDigits(num));
+    }
+}
